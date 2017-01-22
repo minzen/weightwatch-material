1aad85b5b244c3f57051f43b2b4671360b36934b
Feetu Nyrhinen <feetu.nyrhinen@gmail.com>
2017-01-22 01:52:55 +0100

    sample charts

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ### WeightWatch Material
 
-WeightWatch app for viewing weight related data by using Fitbit API, based on Meteor and MaterialCSS by Google. 
+WeightWatch app for viewing weight related data by using Fitbit API, based on Meteor,
+Materialize (http://materializecss.com), and Chartjs (http://www.chartjs.org). 
 
--- client/main.js
+++ client/main.js
@@ -3,3 +3,35 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
+Template.weightInformation.onRendered(function() {
+    var ctx  = document.getElementById("myChart").getContext("2d");
+
+    var rand1 = random();
+    var rand2 = random();
+    var rand3 = random();
+    var data = [
+    {
+        value: rand1,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Red"
+    },
+    {
+        value: rand2,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Green"
+    },
+    {
+        value: rand3,
+        color: "#FDB45C",
+        highlight: "#FFC870",
+        label: "Yellow"
+    }
+    ]
+    var myPieChart = new Chart(ctx).Pie(data);
+});
+
+function random() {
+    return Math.floor((Math.random() * 100) + 1);
+}
